272b34cb9350f98638146babcb69cc20e7a84b82
Roarke Catubig <roarkecatubig@gmail.com>
2018-02-12 14:09:06 -0800

    Slice word bank into individual components and split strings into individual characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,27 @@
 // VARIABLES
 var userGuesses = [];
+var gameWord =[];
+var splitWord = "KAMINO";
+var wordBank = ["KAMINO", "TAKODANA", "KASHYYK"];
 
 
 // FUNCTIONS
+// This function splits up the words in the "wordBank" function and puts them into an array as individual characters. 
+function splitString(s) {
+    var brokenWord = s.split("");
+        //     for (i = 0; i <= sepBank)
+        // var gameWord = splitWord.split("");
+    console.log(brokenWord);
+}
+
+function sliceArray(wordBank) {
+    for (i = 0; i < wordBank.length; i++) {
+        var sepBank = wordBank.slice();
+        splitString(sepBank[i])
+    }
+    console.log(wordBank.slice())
+}
+
 // function initHangman() {
 //     // Records all pressed keys 
 
@@ -16,17 +35,24 @@ var userGuesses = [];
 
 // This function checks to see if inputed character is in the array.
 // function checkArray (guess) {
+    
 //     // Based on the character:
 
-//         // If character is not in the array, push the character to the array.
-//         for (var i = 0; userGuesses.length>0; i++) {
-//             if (userGuesses[i] === guess) {
-//                 userGuesses.pop();
-//                 console.log("Value already in array.")
-//             }
-
+//     // If character is not in the array, push the character to the array.
+//     for (var i = 0; i<userGuesses.length; i++) {
+//         if (userGuesses[i] === guess) {
+//             userGuesses.push(guess);
+//             userGuesses.pop();
+//             console.log("Value already in array.")
 //         }
+
+//         else {
+//             userGuesses.push(guess);
+//         }
+
 //     }
+// }
+
 // If character is in the array, does not push character into the array.
 
 // }
@@ -42,10 +68,6 @@ var userGuesses = [];
 // MAIN PROCESS
 document.onkeyup = function(event) {
     var guess = String.fromCharCode(event.which).toUpperCase();
-    
-    userGuesses.push(guess);
-
-    // checkArray(guess);
-    
+    userGuesses.push(guess)
     console.log(userGuesses)
 }
